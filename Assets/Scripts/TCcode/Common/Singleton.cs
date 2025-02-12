10246ae6d9f5ac070d5efbca630be7ea61c0b75f
bbobbabbi <201944039@itc.ac.kr>
2025-02-12 10:53:02 +0900

    [기능수정]Singleton에서 할당 순서 때문에 메모리상에 Singleton 객체가 Destroy시에도 남아있던 (메모리가 낭비되던) 오류 수정

--- Assets/Scripts/TCcode/Common/Singleton.cs
+++ Assets/Scripts/TCcode/Common/Singleton.cs
@@ -26,13 +26,18 @@ public abstract class Singleton<T> : MonoBehaviour where T : Component
         {
             _instance = this as T;
             DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else 
         { 
             Destroy(gameObject);
         }
         //씬 전환시 호출되는 액션 메서드 할당
-        SceneManager.sceneLoaded += OnSceneLoaded;
+        // 여기서 메소드 구독을 하면 새로 생성된 객체가 하는 일이 생기므로
+        // Destory를 만났더라도 바로지워지지 못하고 요놈이 한번 더 구독되어 
+        // OnSceneLoaded가 호출된다 그리고 지워지고
+        // SceneManager.sceneLoaded += OnSceneLoaded;
     }
     protected abstract void OnSceneLoaded(Scene scene, LoadSceneMode mode);
     
